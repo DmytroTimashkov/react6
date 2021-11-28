7562639f2f9d187988893f6552233c41641ae9e2
lavashikaa <83418132+lavashikaa@users.noreply.github.com>
2021-11-28 18:34:25 +0200

    react5.2

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,21 @@ const copyFile = (done) => {
 
     done()
 };
+const scssFile = (done) => {
+  src('dist/scss/index.scss').pipe(dest('build/styles'));
+  done()
+};
 const readyFile = (done) => {
   console.log('ready');
 
   done();
 };
+const htmlFile = (done) => {
+  src('dist/scss/index.scss').pipe(dest('build/styles'));
+  done()
+};
+
 
 exports.default = parallel(readyFile,copyFile);
+exports.SASS = scssFile;
+exports.HTML = htmlFile;
